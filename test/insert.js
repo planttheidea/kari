e25a765af4b1e2a6b82dd7fa3a7e327f2d190d0a
planttheidea <tony.quetano@planttheidea.com>
2017-06-04 09:00:48 -0400

    Make insert work with objects

--- test/insert.js
+++ test/insert.js
@@ -24,12 +24,42 @@ test('if insert will add multiple items if the item is an array', (t) => {
   const items = ['x', 'y'];
   const index = 2;
 
-  const result = insert(index, items, array);
+  const result = insert(index)(items)(array);
 
   t.not(result, array);
   t.deepEqual(result, [
     ...array.slice(0, index),
     ...items,
     ...array.slice(index)
+  ]);
+});
+
+test('if insert will add the item to the object', (t) => {
+  const object = {
+    foo: 'bar'
+  };
+  const item = 'baz';
+  const key = 'bar';
+
+  const result = insert(key)(item)(object);
+
+  t.not(result, object);
+  t.deepEqual(result, {
+    ...object,
+    [key]: item
+  });
+});
+
+test('if insert will convert items to an array and insert the new value if it is not already an array', (t) => {
+  const value = 'foo';
+  const item = 'bar';
+  const index = 0;
+
+  const result = insert(index)(item)(value);
+
+  t.not(result, value);
+  t.deepEqual(result, [
+    item,
+    value
   ]);
 });
