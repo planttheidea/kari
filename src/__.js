475376c9c2f09da097398d861aec9a2f4f35e602
planttheidea <tony.quetano@planttheidea.com>
2017-06-06 07:29:55 -0400

    Documentation for placeholder

--- src/__.js
+++ src/__.js
@@ -3,4 +3,8 @@ import isFunction from './_utils/isFunction';
 
 const PLACEHOLDER = '__KARI_PARAMETER_PLACEHOLDER__';
 
+/**
+ * @constant {string|Symbol} __ placeholder to use if you want to pass arguments out of order
+ * @default
+ */
 export default isFunction(Symbol) ? Symbol(PLACEHOLDER) : PLACEHOLDER;
